83ae00102482c87d1695cf6a49478722acae0c83
Minosanty <minosantiago17@gmail.com>
2025-06-24 23:35:35 -0300

    se configuro la conexion con la base de datos

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,21 @@
 import { Sequelize } from "sequelize";
 import  dotenv  from "dotenv";
 dotenv.config()
-const sequelize = new Sequelize(
-    process.env.
+
+export const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {  
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    } 
+    
 )
+export const start_DB = async() =>{
+    await sequelize.authenticate()
+     console.log("ok")
+    await sequelize.sync()
+
+
+}
